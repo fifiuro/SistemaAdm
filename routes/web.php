93e74fd8226fde75bfbc135f0fc34fb13b358e12
elliothguti <noelitomalosin@gmail.com>
2019-04-14 17:19:24 -0400

    MOD

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,23 @@ Route::get('/', function () {
     return view('inicio');
 });
 
+/**ACCIONES PARA LE MODULO GESTION */
+//muestra comentario de tgestrion
+Route::get('findGestion','GestionController@index');
+//encia los datos a buscar en la base de datos
+Route::post('findGestion','GestionController@show');
+//mostrara el formulario de nueva gestion
+Route::get('createGestion','GestionController@create');
+//recibe los datos a resgistrar en la base de datos
+Route::post('storeGestion','GestionController@store');
+//envia lso datos para modificar
+Route::get('editGestion','GestionController@edit');
+//envia los datos modificados de la gestion
+Route::post('editGestion','GestionController@update');
+//formulario de confirmacion
+Route::get('confirmGestion','GestionController@confirm');
+//eliminar la gestion
+Route::post('destroyGestion','GestionController@destroy');
 
+
+/**FIN */
