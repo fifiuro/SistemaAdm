1f1ac1da7283cfb1f96e3e2a6f78101a6b28695b
unknown <fifiuro@gmail.com>
2019-04-14 15:16:03 -0400

    modificacion

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,21 @@
+@extends('plantilla.master')
+
+@section('menu')
+    @include('menus.adm')
+@endsection
+
+@section('tituloPag')
+    
+@endsection
+
+@section('subtituloPag')
+    
+@endsection
+
+@section('contenido')
+    
+@endsection
+
+@section('extra')
+
+@endsection
---
+++ resources/views/menus/adm.blade.php
@@ -0,0 +1,120 @@
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-gears"></i>
+        <span>CONFIGURACIONES</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li>
+            <a href="{{ url('findCargo') }}">
+                <i class="fa fa-circle-o"></i> Cargos</a>
+        </li>
+        <li>
+            <a href="{{ url('findDepartamento') }}">
+                <i class="fa fa-circle-o"></i> Departamento</a>
+        </li>
+        <li>
+            <a href="{{ url('findFeriado') }}">
+                <i class="fa fa-circle-o"></i> Feriados</a>
+        </li>
+        <li>
+            <a href="{{ url('findCategoria') }}">
+                <i class="fa fa-circle-o"></i> Tecnologías</a>
+        </li>
+        <li>
+            <a href="{{ url('findAula') }}">
+                <i class="fa fa-circle-o"></i> Aulas</a>
+        </li>
+        <li>
+            <a href="{{ url('findPago') }}">
+                <i class="fa fa-circle-o"></i> Tipos de Pago</a>
+        </li>
+        <li>
+            <a href="{{ url('findDocumento') }}">
+                <i class="fa fa-circle-o"></i> Documentos Respaldo</a>
+        </li>
+    </ul>
+</li>
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-institution"></i>
+        <span>COGNOS</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li>
+            <a href="{{ url('findPersonal') }}">
+                <i class="fa fa-circle-o"></i> Personal Cognos</a>
+        </li>
+        <li>
+            <a href="{{ url('findInstructor') }}">
+                <i class="fa fa-circle-o"></i> Instructores</a>
+        </li>
+        <li>
+            <a href="{{ url('findCurso') }}">
+                <i class="fa fa-circle-o"></i> Cursos</a>
+        </li>
+        <li>
+            <a href="{{ url('findEmpresa') }}">
+                <i class="fa fa-circle-o"></i> Empresas</a>
+        </li>
+    </ul>
+</li>
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-group"></i>
+        <span>CLIENTES</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li>
+            <a href="{{ url('findAlumno') }}">
+                <i class="fa fa-circle-o"></i> Alumno</a>
+        </li>
+        <li>
+            <a href="{{ url('findInteres') }}">
+                <i class="fa fa-circle-o"></i> Interesado</a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="{{ url('findCronograma') }}">
+        <i class="fa fa-calendar"></i>
+        <span>CRONOGRAMA</span>
+    </a>
+</li>
+<li>
+    <a href="{{ url('findInscripcion') }}">
+        <i class="fa fa-child"></i>
+        <span>INSCRIPCIONES</span>
+    </a>
+</li>
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-calendar-check-o "></i>
+        <span>MALLA DE CURSOS</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li>
+            <a href="{{ url('mallaLunesViernes') }}">
+                <i class="fa fa-circle-o"></i> Lunes a viernes</a>
+        </li>
+        <li>
+            <a href="{{ url('mallaSabados') }}">
+                <i class="fa fa-circle-o"></i> Sábado</a>
+        </li>
+        <li>
+            <a href="{{ url('mallaSitio') }}">
+                <i class="fa fa-circle-o"></i> Sitio</a>
+        </li>
+    </ul>
+</li>
