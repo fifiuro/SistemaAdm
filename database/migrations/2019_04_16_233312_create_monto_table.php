b0a2ce5d7f582b922c4a044112b9e104e07b1a63
unknown <fifiuro@gmail.com>
2019-04-16 19:43:54 -0400

    Tabla Monto

---
+++ database/migrations/2019_04_16_233312_create_monto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMontoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monto', function (Blueprint $table) {
+            $table->bigIncrements('id_mon');
+            $table->unsignedBigInteger('id_pro');
+            $table->foreign('id_pro')->references('id_pro')->on('proyecto')->onDelete('cascade');
+            $table->date('fecha');
+            $table->float('monto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monto');
+    }
+}
