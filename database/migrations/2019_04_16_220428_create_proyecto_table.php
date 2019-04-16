31ec8cc27b61c19f6cb651e95695048498f6ae97
unknown <fifiuro@gmail.com>
2019-04-16 18:13:26 -0400

    tabla Proyecto

---
+++ database/migrations/2019_04_16_220428_create_proyecto_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyecto', function (Blueprint $table) {
+            $table->bigIncrements('id_pro');
+            $table->unsignedBigInteger('id_dist');
+            $table->foreign('id_dist')->references('id_dist')->on('distrito')->onDelete('cascade');
+            $table->string('nombre_pro');
+            $table->integer('ema');
+            $table->float('presupuesto');
+            $table->date('fecha_reg');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyecto');
+    }
+}
