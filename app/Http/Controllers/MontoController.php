e65f8844509194377010b16be651d75d883e39d4
unknown <fifiuro@gmail.com>
2019-04-16 23:08:54 -0400

    hola

---
+++ app/Http/Controllers/MontoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Monto;
+use Illuminate\Http\Request;
+
+class MontoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Monto  $monto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Monto $monto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Monto  $monto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Monto $monto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Monto  $monto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Monto $monto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Monto  $monto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Monto $monto)
+    {
+        //
+    }
+}
---
+++ app/Monto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Monto extends Model
+{
+    protected $table ='monto';
+    protected $primaryKey ='id_mon';
+}
